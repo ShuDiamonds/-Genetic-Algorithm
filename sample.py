bf3ad7566ee23e48dc3bd04131cf03d7b736303b
ShuDiamonds <fukuda0729android@gmail.com>
2018-06-29 16:47:36 +0900

    add new file

---
+++ sample.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 08:54:38 2018
+
+@author: shuichi
+"""
+
+import random
+import sys
+sys.path.append('/home/shuichi/anaconda3/lib/python3.6/site-packages')
+
+from deap import base
+from deap import creator
+from deap import tools
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+
+toolbox.register("attr_bool", random.randint, 0, 1)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 100)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+def evalOneMax(individual):
+    return sum(individual),
+
+toolbox.register("evaluate", evalOneMax)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+
+random.seed(64)
+
+pop = toolbox.population(n=300)
+CXPB, MUTPB, NGEN = 0.5, 0.2, 40
+
+print("Start of evolution")
+
+fitnesses = list(map(toolbox.evaluate, pop))
+for ind, fit in zip(pop, fitnesses):
+    ind.fitness.values = fit
+
+print("  Evaluated %i individuals" % len(pop))
+
+for g in range(NGEN):
+    print("-- Generation %i --" % g)
+    
+    offspring = toolbox.select(pop, len(pop))
+    offspring = list(map(toolbox.clone, offspring))
+    
+    for child1, child2 in zip(offspring[::2], offspring[1::2]):
+        if random.random() < CXPB:
+            toolbox.mate(child1, child2)
+            del child1.fitness.values
+            del child2.fitness.values
+    
+    for mutant in offspring:
+        if random.random() < MUTPB:
+            toolbox.mutate(mutant)
+            del mutant.fitness.values
+    
+    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    fitnesses = map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+    
+    print("  Evaluated %i individuals" % len(invalid_ind))
+    
+    pop[:] = offspring
+    
+    fits = [ind.fitness.values[0] for ind in pop]
+    
+    length = len(pop)
+    mean = sum(fits) / length
+    sum2 = sum(x*x for x in fits)
+    std = abs(sum2 / length - mean**2)**0.5
+    
+    print("  Min %s" % min(fits))
+    print("  Max %s" % max(fits))
+    print("  Avg %s" % mean)
+    print("  Std %s" % std)
+
+print("-- End of (successful) evolution --")
+
+best_ind = tools.selBest(pop, 1)[0]
+print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+
